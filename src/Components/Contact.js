db0cd81d743af57f054c8c203b50d7d0db946303
nahida <nahidaadduashshu@gmail.com>
2023-11-21 10:18:36 +0530

    second

---
+++ src/Components/Contact.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+const Contact = () => {
+  return (
+    <div><h2>Contact</h2>
+   <div>
+    <h3>phone number:1234567890</h3>
+    <div><h3>email :</h3></div>
+   </div>
+   </div>
+  )
+}
+
+export default Contact
